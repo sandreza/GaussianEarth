c577e185a2f83392f7b94e8ce7940660cfc868a6
sandreza <andrenogueirasouza@gmail.com>
2024-08-06 15:34:05 -0400

    place float32 extension in right place

--- generate/compute_basis.jl
+++ generate/compute_basis.jl
@@ -1,8 +1,5 @@
 using NCDatasets, LinearAlgebra, Statistics, HDF5, ProgressBars
 
-import Base.Float32
-Float32(a::Missing) = Float32(0.0)
-
 save_directory = "/net/fs06/d3/sandre/GaussianEarthData/"
 data_directory = "/net/fs06/d3/mgeo/CMIP6/interim/"
 
