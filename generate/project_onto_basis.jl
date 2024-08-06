eb03ca9406a916362c58bfe3a801cbd639679875
sandreza <andrenogueirasouza@gmail.com>
2024-08-06 15:27:31 -0400

    add caveat for missing data

--- generate/project_onto_basis.jl
+++ generate/project_onto_basis.jl
@@ -14,37 +14,39 @@ for scenario in ProgressBar(scenario_directories[1:end])
         local_current_path = joinpath(current_path, variable_directory)
         file_names = readdir(local_current_path)
 
-        file_name = file_names[1] # pick the first file for obtaining varaibles
-        file_path = joinpath(local_current_path, file_name)
-
-        ds = Dataset(file_path)
-        field_name = keys(ds)[end]
-        field = Float32.(ds[field_name][:,:,:])
-        M, N, L = size(field)
-        close(ds)
-
-        hfile = h5open(save_directory * field_name * "_basis.hdf5", "r")
-        basis = read(hfile["basis"])
-        metric = read(hfile["metric"])
-        close(hfile)
-
-        projection = zeros(Float32, size(basis)[2], L, length(file_names))
-        global_mean = zeros(Float32, L, length(file_names))
-
-        for (i, file_name) in ProgressBar(enumerate(file_names))
+        if length(file_names) > 0
+            file_name = file_names[1] # pick the first file for obtaining varaibles
             file_path = joinpath(local_current_path, file_name)
+
             ds = Dataset(file_path)
+            field_name = keys(ds)[end]
             field = Float32.(ds[field_name][:,:,:])
             M, N, L = size(field)
-            reshaped_field = reshape(field, M * N, L)
-            projected_field = basis' * reshaped_field
-            projection[:, :, i] .= projected_field
-            global_mean[:, i] .= sum(metric .* field, dims = (1, 2))[:]
-        end
+            close(ds)
 
-        hfile = h5open(save_directory * field_name * "_" * scenario * "_projection.hdf5", "w")
-        hfile["projection"] = projection
-        hfile["global mean"] = global_mean
-        close(hfile)
+            hfile = h5open(save_directory * field_name * "_basis.hdf5", "r")
+            basis = read(hfile["basis"])
+            metric = read(hfile["metric"])
+            close(hfile)
+
+            projection = zeros(Float32, size(basis)[2], L, length(file_names))
+            global_mean = zeros(Float32, L, length(file_names))
+
+            for (i, file_name) in ProgressBar(enumerate(file_names))
+                file_path = joinpath(local_current_path, file_name)
+                ds = Dataset(file_path)
+                field = Float32.(ds[field_name][:,:,:])
+                M, N, L = size(field)
+                reshaped_field = reshape(field, M * N, L)
+                projected_field = basis' * reshaped_field
+                projection[:, :, i] .= projected_field
+                global_mean[:, i] .= sum(metric .* field, dims = (1, 2))[:]
+            end
+
+            hfile = h5open(save_directory * field_name * "_" * scenario * "_projection.hdf5", "w")
+            hfile["projection"] = projection
+            hfile["global mean"] = global_mean
+            close(hfile)
+        end
     end
 end
