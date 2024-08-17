1671813bd592b1ba72bef4d967441ba5f57e57ca
sandreza <andrenogueirasouza@gmail.com>
2024-08-17 15:11:53 -0400

    covariance export

---
+++ generate/export_covariances.jl
@@ -0,0 +1,31 @@
+using CairoMakie
+using NCDatasets, LinearAlgebra, Statistics, HDF5, ProgressBars
+using LinearAlgebra
+
+include("utils.jl")
+##
+save_directory = "/net/fs06/d3/sandre/GaussianEarthData/"
+data_directory = "/net/fs06/d3/mgeo/CMIP6/interim/"
+scenario_directories = readdir(data_directory)
+current_path = joinpath(data_directory, scenario_directories[1])
+variable_directories = readdir(current_path)
+
+##
+field = "tas"
+modes, temperature = concatenate_regression(field, ["historical", "ssp585"])
+##
+regression_indices = [1:139..., 141:251...]
+covsave = h5open(save_directory * field * "_covariances.hdf5", "w")
+covsave["temperature"] = temperature[regression_indices]
+covsave["scale"] = 273
+
+inds = 1:30
+Σs = zeros(Float32, 1000, 1000, length(1:12:size(modes,2)))
+for month in ProgressBar(1:12)
+    for (i,j) in ProgressBar(enumerate(month:12:size(modes,2)))
+        Σs[:,:,i] .= cov(modes[1:1000,j, inds], dims = 2)
+    end
+    covsave["covariances $month"] = Σs[:,:, regression_indices]
+end
+
+close(covsave)
