8f716047cc9f937ed5201572485718e6a17571b9
sandreza <andrenogueirasouza@gmail.com>
2024-08-19 16:18:15 -0400

    ignore tmp files

--- generate/model_and_pattern_scaling_error.jl
+++ generate/model_and_pattern_scaling_error.jl
@@ -7,8 +7,8 @@ scenario_directories = readdir(data_directory)
 current_path = joinpath(data_directory, scenario_directories[1])
 variable_directories = readdir(current_path)
 ##
-field = "tas"
-hfile = h5open(save_directory * field * "_mean_regression.hdf5", "r")
+field_name = "hurs" # ["tas", "hurs"]
+hfile = h5open(save_directory * field_name * "_mean_regression.hdf5", "r")
 regression_coefficients = read(hfile["regression_coefficients 1"])
 linear_coefficients = zeros(Float32, size(regression_coefficients)..., 12)
 for month in ProgressBar(1:12)
@@ -16,7 +16,7 @@ for month in ProgressBar(1:12)
 end
 close(hfile)
 
-Φ = eof_basis("tas")
+Φ = eof_basis(field_name)
 
 averaged_coefficients = mean(linear_coefficients, dims = 3)[:, :, 1]
 
@@ -37,28 +37,28 @@ for i in ProgressBar(1:1000)
     eof_model1000[:, 2] .+= Φ[:, i] * averaged_coefficients[i, 2]
 end
 ##
-include("pattern_scaling_error.jl")
-##
-
-fig = Figure() 
-ax = Axis(fig[1,1])
-ts = [1850:2014, 2015:2100, 2015:2100, 2015:2100]
-for (i, field) in enumerate(fields)
-    Ts = temperatures[i]
-    error_list = [norm((field[:, :, j] .- (linear_fit_yearly[:, :, 1] .+ Ts[j] * linear_fit_yearly[:, :, 2]))[:]) for j in eachindex(Ts)]
-    error_list10 = [norm(reshape(field[:, :, j], size(Φ)[1]) .- (eof_model10[:, 1] .+ Ts[j] * eof_model10[:, 2])) for j in eachindex(Ts)]
-    error_list100 = [norm(reshape(field[:, :, j], size(Φ)[1]) .- (eof_model100[:, 1] .+ Ts[j] * eof_model100[:, 2])) for j in eachindex(Ts)]
-    error_list1000 = [norm(reshape(field[:, :, j], size(Φ)[1]) .- (eof_model1000[:, 1] .+ Ts[j] * eof_model1000[:, 2])) for j in eachindex(Ts)]
-    lines!(ax, ts[i], error_list, label = scenarios[i])
-    lines!(ax, ts[i], error_list10, label = scenarios[i] * " 10 modes")
-    lines!(ax, ts[i], error_list100, label = scenarios[i] * " 100 modes")
-    lines!(ax, ts[i], error_list1000, label = scenarios[i] * " 1000 modes")
+filename  = field_name * "_pattern_scaling.hdf5"
+hfile = h5open(save_directory * filename, "r")
+linear_fit_yearly = read(hfile["linear fit yearly"])
+close(hfile)
+scenarios = ["historical", "ssp585", "ssp119", "ssp245"]
+temperatures = []
+fields = []
+for scenario in scenarios
+    temperature = regression_variable(scenario)
+    a, b = ensemble_averaging(scenario, field_name; ensemble_members = 29)
+    push!(temperatures, temperature)
+    push!(fields, b)
 end
-axislegend(ax; position = :lt)
-save("pattern_scaling_and_model_errors.png", fig)
 
 ##
-yrange = (0, 150)
+i = 2
+Ts = temperatures[i]
+field = fields[i]
+error_list10 = [norm(reshape(field[:, :, j], size(Φ)[1]) .- (eof_model10[:, 1] .+ Ts[j] * eof_model10[:, 2])) for j in eachindex(Ts)]
+ymax = maximum(error_list10)
+
+yrange = (0, ymax)
 fig = Figure(resolution = (2000, 400)) 
 ts = [1850:2014, 2015:2100, 2015:2100, 2015:2100]
 ax = Axis(fig[1,1]; title = "Pattern Scaling", xlabel = "Year", ylabel = "Error")
@@ -96,4 +96,4 @@ for (i, field) in enumerate(fields)
     ylims!(ax, yrange...)
 end
 hideydecorations!(ax, grid = false)
-save("pattern_scaling_and_model_errors_3.png", fig)
+save(field_name * "_pattern_scaling_and_model_errors_3.png", fig)
