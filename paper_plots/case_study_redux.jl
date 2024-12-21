631af5b2da0f2920040a05093124c79fc77e0ef1
m-geo <mgeo@mit.edu>
2024-12-20 21:56:40 -0500

    error difference map

--- paper_plots/case_study_redux.jl
+++ paper_plots/case_study_redux.jl
@@ -25,6 +25,16 @@ close(hfile)
 min_temp = temperatures[1]/273 #2015
 max_temp = temperatures[end]/273 #2100
 
+temps585 = regression_variable("ssp585")
+max_temp585 = temps585[end]
+
+hfile = h5open(save_directory * field * "_basis.hdf5", "r")
+latitude = read(hfile["latitude"])
+longitude = read(hfile["longitude"])
+metric = read(hfile["metric"])
+close(hfile)
+fmetric = reshape(metric, (192*96, 1))
+
 index_1 = [157, 46]
 index_2 = [54, 48]
 index_3 = [19, 87]
@@ -50,23 +60,24 @@ observables = [location_1, location_3]
 fig = Figure(; resolution)
 observable_names_base = [@sprintf("%.2fᵒ, %.2fᵒ ", longitude[tmp[1]], latitude[tmp[2]]) for tmp in [index_1, index_2, index_3]]
 observable_names = observable_names_base .* ["(Jul)"]
-month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+month_string = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+xticks = (1:12, month_string)
+
 for i in eachindex(observables)
     month = 7
     emulator.month[1] = month
-    emulator.global_mean_temperature[1] = min_temp
+    emulator.global_mean_temperature[1] = max_temp585
     emulator_hurs.month[1] = month
-    emulator_hurs.global_mean_temperature[1] = min_temp
+    emulator_hurs.global_mean_temperature[1] = max_temp585
     tasmean, tasstd = emulator_mean_variance_linear_functionals(observables, emulator)
     hursmean, hursstd = emulator_mean_variance_linear_functionals(observables, emulator_hurs)
-
 
     common_options_1 = (; xlabel = "Temperature (K)")
     common_options_2 = (; xlabel = "Relative Humidity (%)")
 
     ax1 = Axis(fig[1, 2*i]; title = observable_names[i], common_options_1..., global_common_options..., ylabel = "PDF")
     xs, ys = gaussian_grid(tasmean[i], tasstd[i])
-    lines!(ax1, xs, ys, color = :blue, linewidth = lw, label = "2015")
+    lines!(ax1, xs, ys, color = :blue, linewidth = lw, label = "SSP5-8.5")
 
     ax2 = Axis(fig[2, 2*i]; title = observable_names[i], common_options_2..., global_common_options..., ylabel = "PDF")
     xs, ys = gaussian_grid(hursmean[i], hursstd[i])
@@ -80,7 +91,7 @@ for i in eachindex(observables)
     hursmean, hursstd = emulator_mean_variance_linear_functionals(observables, emulator_hurs)
 
     xs, ys = gaussian_grid(tasmean[i], tasstd[i])
-    lines!(ax1, xs, ys, color = :orange, linewidth = lw, label = "2100")
+    lines!(ax1, xs, ys, color = :orange, linewidth = lw, label = "New Scenario")
     xs, ys = gaussian_grid(hursmean[i], hursstd[i])
     lines!(ax2, xs, ys, color = :orange, linewidth = lw)
 
@@ -88,25 +99,29 @@ for i in eachindex(observables)
         axislegend(ax1, position = :lt, labelsize = legend_ls)
     end
 
-    ax3 = Axis(fig[1, 2*i-1]; title = observable_names_base[i] * "(2100)", common_options_1..., global_common_options..., ylabel = "Temperature (K)")
-    ax4 = Axis(fig[2, 2*i-1]; title = observable_names_base[i] * "(2100)", common_options_2..., global_common_options..., ylabel = "Relative Humidity (%)")
-    for n in 1:20
-        taslist = zeros(12)
-        hurslist = zeros(12)
-        for month in 1:12
-            emulator.month[1] = month
-            emulator.global_mean_temperature[1] = max_temp
-            emulator_hurs.month[1] = month
-            emulator_hurs.global_mean_temperature[1] = max_temp
-            tas_instance = rand(emulator)
-            hurs_instance = rand(emulator_hurs)
-            
-            taslist[month] = observables[i](tas_instance)
-            hurslist[month] = observables[i](hurs_instance)
-        end
-        lines!(ax3, month_names, taslist, alpha = 0.7)
-        lines!(ax4, month_names, hurslist, alpha = 0.7)
+    ax3 = Axis(fig[1, 2*i-1]; title = observable_names_base[i] * "(2100)", xticks, common_options_1..., global_common_options..., ylabel = "Temperature (K)")
+    ax4 = Axis(fig[2, 2*i-1]; title = observable_names_base[i] * "(2100)", xticks, common_options_2..., global_common_options..., ylabel = "Relative Humidity (%)")
+    tasmeanlist = zeros(Float32, 12)
+    tasstdlist = zeros(Float32, 12)
+    hursmeanlist = zeros(Float32, 12)
+    hursstdlist = zeros(Float32, 12)
+    for month in 1:12
+        emulator.month[1] = month
+        emulator.global_mean_temperature[1] = max_temp
+        tasmean, tasstd = emulator_mean_variance_linear_functionals([observables[i]], emulator)
+        emulator_hurs.month[1] = month
+        emulator_hurs.global_mean_temperature[1] = max_temp
+        hursmean, hursstd = emulator_mean_variance_linear_functionals([observables[i]], emulator_hurs)
+        
+        tasmeanlist[month] = tasmean[1]
+        hursmeanlist[month] = hursmean[1]
+        tasstdlist[month] = tasstd[1]
+        hursstdlist[month] = hursstd[1]
     end
+    lines!(ax3, 1:12, tasmeanlist, color = :orange, linewidth = lw)
+    band!(ax3, 1:12, tasmeanlist .- 3 .* tasstdlist, tasmeanlist .+ 3 .* tasstdlist, color = (:orange, 0.2))
+    lines!(ax4, 1:12, hursmeanlist, color = :orange, linewidth = lw)
+    band!(ax4, 1:12, hursmeanlist .- 3 .* hursstdlist, hursmeanlist .+ 3 .* hursstdlist, color = (:orange, 0.2))
 
 end
 display(fig)
